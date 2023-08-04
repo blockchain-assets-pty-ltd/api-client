56ce944f0bd3efb62109734bc4157d628e96d573
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-08-04 11:36:45 +0800

    Updated outdated packages

--- src/signing.ts
+++ src/signing.ts
@@ -2,8 +2,6 @@ import Web3 from "web3"
 
 export const signMessageWithEthereumPrivateKey = async (message: string, privateKey: string) => {
     const web3 = new Web3()
-    const address = web3.eth.accounts.wallet.add(privateKey).address
-    const signature = await web3.eth.sign(message, address)
-
+    const signature = web3.eth.accounts.sign(message, privateKey)
     return signature
 }
