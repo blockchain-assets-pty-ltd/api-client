605991fdc4ddaf9b4a1cd1b9c8aba7b1ae543915
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-08-14 09:24:55 +0800

    Offline signing fix

--- src/signing.ts
+++ src/signing.ts
@@ -1,7 +1,7 @@
-import Web3 from "web3"
+import { Web3 } from "web3"
 
 export const signMessageWithEthereumPrivateKey = (message: string, privateKey: string): string => {
     const web3 = new Web3()
-    const signResult = web3.eth.accounts.sign(message, privateKey)
+    const signResult = web3.eth.accounts.sign(message, "0x" + privateKey)
     return signResult.signature
 }
