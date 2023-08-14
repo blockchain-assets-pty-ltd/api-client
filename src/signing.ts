5e76519b0449447838c0426156db738d65f57fef
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-08-14 14:05:59 +0800

    Suppressed web3 provider warning

--- src/signing.ts
+++ src/signing.ts
@@ -1,7 +1,7 @@
 import { Web3 } from "web3"
 
 export const signMessageWithEthereumPrivateKey = (message: string, privateKey: string): string => {
-    const web3 = new Web3()
+    const web3 = new Web3("http://") // Fake URL suppresses 'no provider' warning.
     const signResult = web3.eth.accounts.sign(message, "0x" + privateKey)
     return signResult.signature
 }
